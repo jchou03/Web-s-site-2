54187cdd9bfcde7e7c08c189badc7d898e61dea2
Jared <jaredhappy@gmail.com>
2019-05-31 14:48:23 -0700

    made it so al ptags in a container named "hide" will automatically be assigned the hideOnStart class to be hiden and shown one at a time

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var shown = 0;
+    $(".hide > p").addClass("hideOnStart");
     $(".hideOnStart").hide();
+    $("#3").hide();
     $(".content").css("background-color","aqua");
     $("#button").click(function(){
         console.log(shown);

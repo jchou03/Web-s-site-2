ca95a38bb83bb00da4fb40333bc90f96d3c5aa50
Jared <jaredhappy@gmail.com>
2019-05-31 14:00:42 -0700

    propperly linked the javascript

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var shown = 0;
+    $("p").hide();
+    $(".content").css("background-color","aqua");
     $("#button").click(function(){
-        $(".content").append($(""))
+        $(".content").append("<p>herro</p>")
     })
 })

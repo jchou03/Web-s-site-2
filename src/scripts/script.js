d64d5ace2784ddc175ae565176f3ca1ba35ec1c9
Jared <jaredhappy@gmail.com>
2019-05-31 14:23:42 -0700

    no longer hides any p tags in the body

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var shown = 0;
-    $("p").hide();
+    // $("p").hide();
     $(".content").css("background-color","aqua");
     $("#button").click(function(){
         $(".content").append("<p>herro</p>")

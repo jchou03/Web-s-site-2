62dd89a0074d2332f0d0fca34f1ca6cd2a39c4ea
Dominic-b <42015317+Dominic-b@users.noreply.github.com>
2019-06-04 12:55:07 -0700

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
     var shown = 0;
-    $(".hide > p").addClass("hideOnStart");
-    $(".hideOnStart").hide();
+    $(".hide > p").addClass("hideOnStart");//adds p in hide div
+    $(".hide > button").addClass("hideOnStart");//adds button in hide div
+    $(".hideOnStart").hide();//hides all in hideonstart class
     $("#3").hide();
-    $(".content").css("background-color","aqua");
-    $("#button").click(function(){
+    $(".content").css("background-color","aqua");//sets background color to aqua
+    
+    $(".next").click(function(){//for buttons of the next class, it cycles through
         console.log(shown);
         $("#" + shown).hide();
         shown += 1;
         $("#" + shown).fadeIn();
     })
+    
+    $(".option1").click(function(){//for buttons of the option1 class, it goes forwards 1
+        console.log(shown);
+        $("#" + shown).hide();
+        shown += 1;
+        $("#" + shown).fadeIn();
+    })
+    
+    $(".option2").click(function(){//for buttons of the option2 class, it goes forward 5
+        console.log(shown);
+        $("#" + shown).hide();
+        shown += 6;//goes 1 ahead
+        $("#" + shown).fadeIn();
+    })
+    
 })

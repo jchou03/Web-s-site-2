c58a7aa4b5fd16c27329b0df70f4eb6c73685f2c
dbert <dbert@DESKTOP-80U0992>
2019-06-04 13:42:46 -0700

    made previous paragraphs disappear on pressing a button

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
     $(".hide > p").addClass("hideOnStart");
     $(".hideOnStart").hide();
     // when clicking the button, show the next part of the story
-    $(".hide > p").addClass("hideOnStart");//adds p in hide div
-    $(".hideOnStart").hide();//hides all in hideonstart class
+	$("#" + shown).fadeIn();
     
     $(".next").click(function(){//for buttons of the next class, it cycles through
         console.log(shown);
@@ -16,15 +15,17 @@ $(document).ready(function(){
     
     $(".option1").click(function(){//for buttons of the option1 class, it goes forwards 1
         console.log(shown);
-        $("#" + shown).hide();
-        shown += 1;
+		shown += 1;
+		for (var i = 0; i < shown; i++) {
+			$("#" + i).hide();
+		}
         $("#" + shown).fadeIn();
     })
     
     $(".option2").click(function(){//for buttons of the option2 class, it goes forward 5
         console.log(shown);
         $("#" + shown).hide();
-        shown += 6;//goes 1 ahead
+        shown += 6;//goes 5 ahead
         $("#" + shown).fadeIn();
     })
     

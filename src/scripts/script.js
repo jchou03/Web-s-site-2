0662ba3986d0061bcb2eb8490e42bcf57fce6153
Jared <jaredhappy@gmail.com>
2019-06-04 13:48:04 -0700

    added path selection by id rather than changing the id to a larger number

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,31 +1,38 @@
 $(document).ready(function(){
     var shown = 0;
+    var path = "";
+    var id= "#"+shown;
     // hide all of the text, but make it so each one can individually be shown
     $(".hide > p").addClass("hideOnStart");
     $(".hideOnStart").hide();
-    // when clicking the button, show the next part of the story
-    $(".hide > p").addClass("hideOnStart");//adds p in hide div
-    $(".hideOnStart").hide();//hides all in hideonstart class
     
     $(".next").click(function(){//for buttons of the next class, it cycles through
-        console.log(shown);
-        // $("#" + shown).hide();
+        if(path != ""){
+            id = "#"+ path + shown;
+        }else{
+            id= "#" + shown;
+        }
+        console.log(id);
+        $(id).fadeIn();
         shown += 1;
-        $("#" + shown).fadeIn();
     })
-    
+    if(path == "normal"){
+        console.log("banana");
+    }
     $(".option1").click(function(){//for buttons of the option1 class, it goes forwards 1
-        console.log(shown);
-        $("#" + shown).hide();
+        path = "normal";
+        id = "#"+ path + shown;
+        console.log(id);
         shown += 1;
-        $("#" + shown).fadeIn();
+        $(id).fadeIn();
     })
     
     $(".option2").click(function(){//for buttons of the option2 class, it goes forward 5
-        console.log(shown);
-        $("#" + shown).hide();
-        shown += 6;//goes 1 ahead
-        $("#" + shown).fadeIn();
+        path = "depression";
+        id = "#"+ path + shown;
+        console.log(id);
+        $(id).fadeIn();
+        shown += 1;
     })
     
 })
